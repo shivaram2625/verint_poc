f19e75d3e3ed07834cbdf566719b7705ae88652b
shivaram <shivaram.mamidi@bpmlinks.com>
2026-01-30 13:40:44 +0530

    secrets updates

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from GitHub Actions → Elastic Beanstalk POC!123"
+    return "Hello from GitHub Actions → Elastic Beanstalk Verint POC!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5005)
